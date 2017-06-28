0bad97bc22f517c3c077b4a22713250e75caa387
panagiotis <panagiotis>
2017-06-29 01:52:52 +0300

    Add project files.

---
+++ AsyncTest1/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.sendMail_btn = New System.Windows.Forms.Button()
+        Me.output_tb = New System.Windows.Forms.TextBox()
+        Me.another_procedure_btn = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.SuspendLayout()
+        '
+        'sendMail_btn
+        '
+        Me.sendMail_btn.Location = New System.Drawing.Point(12, 12)
+        Me.sendMail_btn.Name = "sendMail_btn"
+        Me.sendMail_btn.Size = New System.Drawing.Size(125, 23)
+        Me.sendMail_btn.TabIndex = 0
+        Me.sendMail_btn.Text = "Send Mail"
+        Me.sendMail_btn.UseVisualStyleBackColor = True
+        '
+        'output_tb
+        '
+        Me.output_tb.Location = New System.Drawing.Point(277, 13)
+        Me.output_tb.Multiline = True
+        Me.output_tb.Name = "output_tb"
+        Me.output_tb.Size = New System.Drawing.Size(228, 236)
+        Me.output_tb.TabIndex = 1
+        '
+        'another_procedure_btn
+        '
+        Me.another_procedure_btn.Location = New System.Drawing.Point(12, 41)
+        Me.another_procedure_btn.Name = "another_procedure_btn"
+        Me.another_procedure_btn.Size = New System.Drawing.Size(125, 23)
+        Me.another_procedure_btn.TabIndex = 2
+        Me.another_procedure_btn.Text = "Another Procedure"
+        Me.another_procedure_btn.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 225)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(259, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(517, 261)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.another_procedure_btn)
+        Me.Controls.Add(Me.output_tb)
+        Me.Controls.Add(Me.sendMail_btn)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents sendMail_btn As Button
+    Friend WithEvents output_tb As TextBox
+    Friend WithEvents another_procedure_btn As Button
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+End Class
---
+++ AsyncTest1/Form1.vb
@@ -0,0 +1,38 @@
+﻿Imports System.ComponentModel
+Imports System.Threading
+Public Class Form1
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub another_procedure_btn_Click(sender As Object, e As EventArgs) Handles another_procedure_btn.Click
+        output_tb.AppendText("Another procedure executed" + Environment.NewLine)
+
+    End Sub
+
+
+
+    Private Sub sendMail_btn_Click(sender As Object, e As EventArgs) Handles sendMail_btn.Click
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        For i = 0 To 100
+            Thread.Sleep(40)
+            BackgroundWorker1.ReportProgress(i)
+        Next
+
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        output_tb.AppendText("Email is send baby :)" + Environment.NewLine)
+    End Sub
+End Class
